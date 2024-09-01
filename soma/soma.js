8e43e9b900627727715cf51368577583da4d1ea6
Guilherme Alves <97guilherme.alves@gmail.com>
2024-09-01 14:26:48 -0300

    :heavy_check_mark:test sum

---
+++ soma/soma.js
@@ -0,0 +1,8 @@
+var indice = 12;
+var soma = 0;
+var k = 1;
+while (k < indice) {
+    k = k + 1;
+    soma = soma + k;
+}
+console.log(soma);
---
+++ soma/soma.ts
@@ -0,0 +1,10 @@
+let indice: number = 12;
+let soma: number = 0;
+let k: number = 1;
+
+while (k < indice) {
+  k = k + 1;
+  soma = soma + k;
+}
+
+console.log(soma);
